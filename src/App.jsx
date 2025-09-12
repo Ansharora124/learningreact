d2436b0433694d4563c98d19940a76d33b5d795b
Ansharora124 <ansharora13b@gmail.com>
2025-09-12 12:27:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("")
+
+  const handleInputChange = (event) => {
+    // update state so the UI reflects typing
+    setInputValue(event.target.value)
+    console.log(event.target.value)
+  }
+
+  return (
+    <div className='App'>
+      <input type='text' value={inputValue} onChange={handleInputChange} />
+      {inputValue}
+    </div>
+  )
+}
+
+export default App
