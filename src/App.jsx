fbd1159dac7d2a255649d81d695dc49f82c1f93f
Ansharora124 <ansharora13b@gmail.com>
2025-09-21 19:06:10 +0530

    component life cycle and use effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,67 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Text} from "./Text"
 function App() {
-const [todoList,setTodoList]=useState([]);
-const [newTask,setNewTask]=useState("");
 
-const handleChange=(event)=>{
-setNewTask(event.target.value);
-};
+const [showText,setShowText]=useState(false);
 
 
 
-const addTask = () =>{
-  const task={
-    id:todoList.length===0 ? 1: todoList[todoList.length-1].id+1,
-    taskname:newTask,
 
-  };
-
-setTodoList([...todoList,task]);
-};
+  
+  
  
-const deleteTask=(id)=>{
-  
-  
- setTodoList(todoList.filter((task)=> task.id !==id))
-};
 
-const completeTask=(id)=>{
-  setTodoList(
-    todoList.map((task)=>{
-      if(task.id===id){
-        return {...task,completed:true};
-      }
-      else{
-        return task;
-      }
-    })
-  )
 
-};
 
 return (
    
 <div className='App'>
+<button onClick={()=>{
+  setShowText(!showText);
 
- <div className='addtask'>
-    <input onChange={handleChange} />
-    <button onClick={addTask}>press</button>
-    
-   </div>
-  <div className='list'>
-    {todoList.map((task)=>{
-      return (
-      <div>
-       <h1 className={task.completed ? "completed" : ""}>{task.taskname}</h1>
-
-        <button onClick={()=>deleteTask(task.id)}>X</button>
-        <button onClick={()=>completeTask(task.id)}>task completed</button>
-      </div>)
-    })}
-  </div>
+}}>
+  show text
+</button>
+ {showText&&<Text/>}
 
 </div>
   
---
+++ src/Text.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState,useEffect } from "react";
+
+
+export const Text =()=>{
+const [text,setText] =useState("");
+
+useEffect(()=>{
+    console.log("mounted");
+return ()=>{
+    console.log("unmounted");
+}
+},[]);
+
+
+return(
+<div>
+<input 
+
+onChange={(event)=>{
+    setText(event.target.value);
+}}
+/>
+
+
+<h1>{text}</h1>
+</div>
+);
+
+};
