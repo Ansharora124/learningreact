dc7a24b1346431bdb525abddf59406030e7cb747
Ansharora124 <ansharora13b@gmail.com>
2025-10-28 20:30:44 +0530

    self made to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import './App.css'
-import { Form } from './components/Form'
-
-
-
-
+import './App.css';
+import React, {useState} from 'react';
 
 
 function App() {
 
+ const [inputText,setInputText]=useState(""); 
+const [work,setWork]=useState([]);
 
+
+
+
+const inputChange=(e)=>{
+  setInputText(e.target.value);
+};
+
+const updateWork=()=>{
+ if(inputText.trim()!==""){
+  setWork([...work,inputText]);
+  setInputText("");
+ }
+};
+
+const deleteWork=(index)=>{
+  setWork(work.filter((_,i)=>i!==index));
+
+};
 
 
 
   return (
     <div className='App'>
  
-<Form/>
+ <p>To do list</p> <input type="text" placeholder='work to done' value={inputText} onChange={inputChange}/>
+ <button value={work} onClick={updateWork}>update</button> 
+ <ul>{work.map((work,index)=>(
+  <li key={index}>{`${work}`}   <button onClick={()=>deleteWork(index)}>delete shit</button></li>
+))
+
+}
+</ul>
+
+ 
+
 
       
       
